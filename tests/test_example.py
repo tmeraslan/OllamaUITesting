e4960f5f84e3ba9fbed73faf610e8e8feb325fcb
tamer <tmeraslan1@gmail.com>
2025-08-19 21:18:15 +0300

    wworkflow

--- tests/test_example.py
+++ tests/test_example.py
@@ -17,7 +17,8 @@ class ExampleTestCase(unittest.TestCase):
         options = Options()
         if os.getenv("HEADLESS", "false").lower() == "true":
             options.add_argument("--headless=new")
-            
+       
+           
 
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")

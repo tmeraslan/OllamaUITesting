2fc0a01ad02a78ef906f7acfee811355a39f55ff
tamer <tmeraslan1@gmail.com>
2025-08-19 21:28:27 +0300

    fix

--- tests/test_example.py
+++ tests/test_example.py
@@ -3,6 +3,9 @@ import os
 import unittest
 from selenium import webdriver
 from pages.chat_page import ChatPage
+from selenium.webdriver.chrome.options import Options
+import shutil
+import tempfile
 
 
 
@@ -13,11 +16,23 @@ class ExampleTestCase(unittest.TestCase):
     
     def setUp(self):
         options = Options()
+        if os.getenv("HEADLESS", "false").lower() == "true":
+            options.add_argument("--headless=new")
+            
+
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        # # Create a temporary profile folder -user data
+        self.user_data_dir = tempfile.mkdtemp(prefix=f"chrome-user-data-uid-{time.time()}")
         options.add_argument(f"--user-data-dir={self.user_data_dir}")
-        self.driver = webdriver.Chrome()
+
+        # # It's important to add the window-size before creating the driver
+        
+        self.driver = webdriver.Chrome(options=options)
 
     def tearDown(self):
         self.driver.quit()
+        shutil.rmtree(self.user_data_dir, ignore_errors=True)
 
 
     def test_sent_message(self):

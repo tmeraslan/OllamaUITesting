bf8d4a9dd909b7a8f4f5b04bf0a2b0934373876d
tamer <tmeraslan1@gmail.com>
2025-08-19 21:15:31 +0300

    fix

--- tests/test_example.py
+++ tests/test_example.py
@@ -2,6 +2,10 @@ import os
 import unittest
 from selenium import webdriver
 from pages.chat_page import ChatPage
+from selenium.webdriver.chrome.options import Options
+import tempfile
+import shutil
+
 
 
 OLLAMA_URL = os.environ.get('OLLAMA_URL', 'http://localhost:3000')  # Default to localhost if not set
@@ -10,10 +14,24 @@ class ExampleTestCase(unittest.TestCase):
     
     
     def setUp(self):
-        self.driver = webdriver.Chrome()
+        options = Options()
+        if os.getenv("HEADLESS", "false").lower() == "true":
+            options.add_argument("--headless=new")
+            
+
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        # # Create a temporary profile folder -user data
+        self.user_data_dir = tempfile.mkdtemp(prefix=f"chrome-user-data-uid-{time.time()}")
+        options.add_argument(f"--user-data-dir={self.user_data_dir}")
+
+        # # It's important to add the window-size before creating the driver
+        
+        self.driver = webdriver.Chrome(options=options)
 
     def tearDown(self):
         self.driver.quit()
+        shutil.rmtree(self.user_data_dir, ignore_errors=True)
 
 
     def test_sent_message(self):

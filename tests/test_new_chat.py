2ae76472440d61067dd75c3f53815c2fcc7f9468
tamer <tmeraslan1@gmail.com>
2025-08-20 17:13:42 +0300

    addtest

--- tests/test_new_chat.py
+++ tests/test_new_chat.py
@@ -1,22 +1,22 @@
-# tests/test_new_chat.py
-import unittest
-import os
-from drivers.driver_factory import get_driver
-# from pages.chat_page import ChatPage
-from pages.page_factory import PageFactory 
+# # tests/test_new_chat.py
+# import unittest
+# import os
+# from drivers.driver_factory import get_driver
+# # from pages.chat_page import ChatPage
+# from pages.page_factory import PageFactory 
 
-OLLAMA_URL = os.environ.get('OLLAMA_URL', 'http://localhost:3000')
+# OLLAMA_URL = os.environ.get('OLLAMA_URL', 'http://localhost:3000')
 
-class ExampleTestCase(unittest.TestCase):
-    def setUp(self):
-        self.driver = get_driver()   # ← שימוש ב-DriverFactory דרך get_driver
+# class ExampleTestCase(unittest.TestCase):
+#     def setUp(self):
+#         self.driver = get_driver()   # ← שימוש ב-DriverFactory דרך get_driver
 
-    def tearDown(self):
-        self.driver.quit()
+#     def tearDown(self):
+#         self.driver.quit()
 
-    def test_sent_message(self):
-        self.driver.get(OLLAMA_URL)
-        chat = PageFactory.create_chat_page(self.driver)
-        chat.click_SIDEBAR_BUTTON()
+#     def test_sent_message(self):
+#         self.driver.get(OLLAMA_URL)
+#         chat = PageFactory.create_chat_page(self.driver)
+#         chat.click_SIDEBAR_BUTTON()
 
         
